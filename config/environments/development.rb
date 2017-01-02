e1e122a1c5167be2964861a0c4a65d63a5bc70a2
Ziv Zamechek <ziv921@yahoo.com>
2017-01-02 13:09:05 -0500

    Add email with sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,20 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.delivery_method = :smtp
+  
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :authentication => :plain,
+    :user_name => "do-not-reply@example.com",
+    :password => ENV["SMTP_ENTRY"],
+    :enable_starttls_auto => true
+  }
+
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
